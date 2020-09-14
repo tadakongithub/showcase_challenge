5d7ae1739ee38c18a43b1d3ec3eacc597762ad34
Tadashi Konno <jensonnumber1@gmail.com>
2020-09-14 16:35:07 -0700

    background dim down when Add new education button is clicked.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import styled from 'styled-components'
 
 const Modal = (props) => {
-    const display = props.showModal ? 'flex' : 'none'
+    const display = props.showModal ? 'flex' : 'none';
+
+    const Container = styled.div`
+        position: absolute;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100vw;
+        height: 100vh;
+        background-color: rgba(0,0,0,.3);
+        z-index: 1;
+        align-items: center;
+        justify-content: center;
+        display: ${display}
+    `
     return (
-        <div style={{display: display}}>
+        <Container>
             hi
-        </div>
+        </Container>
     )
 }
 
