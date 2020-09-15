079ca3a43e25bfe784ce76969330c9bde396926b
Tadashi Konno <jensonnumber1@gmail.com>
2020-09-14 18:28:42 -0700

    input field for university is created.
    Styled components for modal container and form were moved outside Modal component.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,7 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 
-const Modal = (props) => {
-    const display = props.showModal ? 'flex' : 'none';
-
-    const Container = styled.div`
+const Container = styled.div`
         position: absolute;
         top: 0;
         right: 0;
@@ -16,12 +13,34 @@ const Modal = (props) => {
         z-index: 1;
         align-items: center;
         justify-content: center;
-        display: ${display}
     `
+
+    const Form = styled.form`
+        width: 500px;
+        height: 400px;
+        background-color: #fff;
+    `
+
+const Modal = (props) => {
+
+    const [university, setUniversity] = useState('')
+
+    const display = props.showModal ? 'flex' : 'none';
+
+    
+
+    const handleChange = (e) => {
+        setUniversity(e.target.value)
+        console.log(university)
+    }
+
     return (
-        <Container>
-            hi
-        </Container>
+        <Container style={{display: display}}>
+            <Form>
+                <label>University</label>
+                <input type="text" value={university} onChange={handleChange} />
+            </Form>
+        </Container> 
     )
 }
 
