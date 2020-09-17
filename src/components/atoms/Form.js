3a7a721e91dffd78a4b7a9895849a146fe160764
Tadashi Konno <jensonnumber1@gmail.com>
2020-09-17 03:46:01 -0700

    overflow value was fixed in form component.
    It was changed from scroll to auto. Auto is similar to scroll, but it adds scroll bar only when necessary

--- src/components/atoms/Form.js
+++ src/components/atoms/Form.js
@@ -2,12 +2,12 @@ import styled from 'styled-components'
 
 const Form = styled.form.attrs(props => ({
     width: props.width || 'auto',
-    height: props.height || 'auto'
+    height: props.height || 'auto',
 }))`
     width: ${props => props.width};
     height: ${props => props.height};
     padding: 15px;
-    overflow: scroll;
+    overflow: auto;
     background-color: #fff;
 `
 
