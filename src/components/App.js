7a7911c5c43c87acf8a61be4a5df72940555ed35
Tadashi Konno <jensonnumber1@gmail.com>
2020-09-17 19:33:13 -0700

    api call to fetch all universities was moved.
    It was moved from Main to App for the following reasons:
    
    - While user is typing their name, app can still  fetch data asynchornously. This reduces the waiting time to for user to see main page, none  if it  finishes loading before user submit their name.
    - If api call is implemented in component og higher  hierarchy, the data could be used in other child  components as well.

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,32 @@
-import React, {useState } from 'react'
+import React, {useState,  useEffect } from 'react'
+import axios from 'axios'
 import Intro from './pages/Intro'
 import Main from './pages/Main'
 
 const App = () => {
 
     const [userName, setUserName] = useState('')
+    const [allUniversities, setAllUniversities] = useState([])
+    const [isLoading, setIsLoading] = useState(false)
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+
+            const result = await axios(`http://universities.hipolabs.com/search`)
+
+            setAllUniversities(result.data)
+            console.log('loaded')
+            setIsLoading(false)
+        }
+        fetchData()
+    }, [])
 
     const setName = (userName) => {
         setUserName(userName)
     }
 
-    return userName === '' ? <Intro setName={setName}/> : <Main userName={userName}/>
+    return userName === '' ? <Intro setName={setName}/> : <Main userName={userName} allUniversities={allUniversities} isLoading={isLoading}/>
     
 }
 
--- src/components/pages/Main.js
+++ src/components/pages/Main.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React, { useState } from 'react'
+import { FlexBox } from '../atoms/FlexBox'
 import Modal from '../organisms/Modal'
 import MainHeader from '../organisms/MainHeader'
 import MainBody from '../organisms/MainBody'
@@ -8,20 +8,7 @@ const Main = (props) => {
 
     const [showModal, setShowModal] = useState(false)
     const [education, setEducation] = useState([])
-    const [allUniversities, setAllUniversities] = useState([])
-    const [isLoading, setIsLoading] = useState(false)
-
-    useEffect(() => {
-        const fetchData = async () => {
-            setIsLoading(true);
-
-            const result = await axios(`http://universities.hipolabs.com/search`)
-
-            setAllUniversities(result.data)
-            setIsLoading(false)
-        }
-        fetchData()
-    }, [])
+    
 
     const handleSave = (university, degree, study, startYear, endYear, gpa, description) => {
         const newEducation = education.concat({
@@ -42,15 +29,15 @@ const Main = (props) => {
         setShowModal(false)
     }
 
-    if(isLoading){
-        return <div>Loading ...</div>
+    if(props.isLoading){
+        return <FlexBox height="100vh"><div>Loading ...</div></FlexBox>
     }
 
     return (
         <React.Fragment>
             <MainHeader userName={props.userName} handleClick={() => setShowModal(true)}/>
             <MainBody education={education} />
-            <Modal showModal={showModal} closeModal={closeModal} allUniversities={allUniversities} handleSave={handleSave} />
+            <Modal showModal={showModal} closeModal={closeModal} allUniversities={props.allUniversities} handleSave={handleSave} />
         </React.Fragment> 
     )
 }
